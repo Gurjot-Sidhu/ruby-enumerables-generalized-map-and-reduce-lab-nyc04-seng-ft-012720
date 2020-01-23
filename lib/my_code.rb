58136bd26d07ba57fb3d9549a0d5cf0bd2cd3d41
Gurjot Sidhu <gurjotsidhu4@gmail.com>
2020-01-23 06:47:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,23 @@
 # Your Code Here
+def map(array)
+  new = []
+  i = 0 
+  while i < array.length do
+    new.push(yield(array[i]))
+    i+=1 
+  end
+  new
+end
+
+def reduce(array,starting_value = nil)
+  if starting_value
+    num1 = starting_value
+    i = 0 
+  else
+    num1 =array[0]
+    i=0 
+  end
+  while i < array.length
+  
+end
+end
